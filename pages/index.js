6a002cc7ca71532822de3c0db5a13e44ab9d636e
Anderson Jorge <anderson.jorge33@gmail.com>
2022-01-07 12:40:40 -0300

    criação dos components de navbar, footer e layout

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>PokeNext</title>
-        <link rel="icon" href="images/favicon.ico" />
-      </Head>
       <h1>PokeNext</h1>
     </>
   )
